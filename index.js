a6f3261db6e690b702a5e91e4ee3a2fa0b9baf28
Gavin <gavin@mindthetic.com>
2018-01-05 14:53:55 +0000

    Fixed linting issue

--- index.js
+++ index.js
@@ -1,84 +1,80 @@
-import postcss from 'postcss';
-import valueParser from 'postcss-value-parser';
+import postcss from "postcss";
+import valueParser from "postcss-value-parser";
 
-module.export = postcss.plugin('postcss-negative-padding', () => {
-    // opts = opts || {};
+export default postcss.plugin("postcss-negative-padding", () => {
+	// opts = opts || {};
 
-    var padding = function (decl) {
+	var padding = function(decl) {
+		var values = postcss.list.space(decl.value);
 
-        var values = postcss.list.space(decl.value);
+		const properties = ["top", "right", "bottom", "left"];
 
-        const properties = ['top', 'right', 'bottom', 'left'];
+		if (decl.prop === "padding") {
+			if (values.length === 1) {
+				values.push(values[0]);
+			}
+			if (values.length === 2) {
+				values.push(values[0]);
+			}
+			if (values.length === 3) {
+				values.push(values[1]);
+			}
 
-        if (decl.prop === 'padding') {
+			properties.forEach((side, index) => {
+				const number = valueParser.unit(values[index]).number;
+				const unit = valueParser.unit(values[index]).unit;
 
-            if (values.length === 1) {
-                values.push(values[0]);
-            }
-            if (values.length === 2) {
-                values.push(values[0]);
-            }
-            if (values.length === 3) {
-                values.push(values[1]);
-            }
+				// Create padding property for each side
+				decl.before({
+					prop: `padding-${side}`,
+					value: Math.abs(number) + unit
+				});
+			});
 
-            properties.forEach((side, index) => {
-                    const number = valueParser.unit(values[index]).number
-                    const unit = valueParser.unit(values[index]).unit
+			properties.forEach((side, index) => {
+				const number = valueParser.unit(values[index]).number;
+				// const unit = valueParser.unit(values[index]).unit
+				// const negative = number < 0 ? true : false;
+				const negative = number < 0;
 
-                    // Create padding property for each side
-                    decl.before({
-                        prop:  `padding-${ side }`,
-                        value: Math.abs(number) + unit
-                    });
-    		});
+				// Create margin property for each side if value negative
+				if (negative) {
+					decl.before({
+						prop: `margin-${side}`,
+						value: values[index]
+					});
+				}
+			});
+		} else {
+			const re = /(padding)-(top|right|bottom|left)/i;
+			var side = decl.prop.match(re)[2];
 
-            properties.forEach((side, index) => {
-                    const number = valueParser.unit(values[index]).number
-                    // const unit = valueParser.unit(values[index]).unit
-                    // const negative = number < 0 ? true : false;
-                    const negative = number < 0;
+			const number = valueParser.unit(decl.value).number;
+			const unit = valueParser.unit(decl.value).unit;
+			// const negative = number < 0 ? true : false;
+			const negative = number < 0;
 
-                    // Create margin property for each side if value negative
-                    if (negative) {
-                        decl.before({
-                            prop:  `margin-${ side }`,
-                            value: values[index]
-                        });
-                    }
-            });
-        }
-        else {
-            const re = /(padding)-(top|right|bottom|left)/i;
-            var side = decl.prop.match(re)[2];
+			decl.before({
+				prop: `padding-${side}`,
+				value: Math.abs(number) + unit
+			});
 
-            const number = valueParser.unit(decl.value).number;
-            const unit = valueParser.unit(decl.value).unit;
-            // const negative = number < 0 ? true : false;
-            const negative = number < 0;
+			if (negative) {
+				decl.before({
+					prop: `margin-${side}`,
+					value: decl.value
+				});
+			}
+		}
 
-            decl.before({
-                prop:  `padding-${ side }`,
-                value: Math.abs(number) + unit
-            });
+		decl.remove();
+	};
 
-            if (negative) {
-                decl.before({
-                    prop:  `margin-${ side }`,
-                    value: decl.value
-                });
-            }
-        }
+	return function(css) {
+		const propertyMatch = new RegExp(`^(padding)`);
 
-        decl.remove();
-    }
-
-    return function (css) {
-
-        const propertyMatch = new RegExp(`^(padding)`);
-
-        css.walkDecls(propertyMatch, decl => {
-  			padding(decl);
+		css.walkDecls(propertyMatch, decl => {
+			padding(decl);
 		});
-    };
-  });
+	};
+});
